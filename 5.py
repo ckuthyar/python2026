2f25754f972b9f4fcdf6cc0f10a71c23b67f8203
ckuthyar <ckuthyar@gmail.com>
2026-01-01 22:27:52 +0530

    Take a 4 digit number and reverse it

--- 5.py
+++ 5.py
@@ -1,23 +1,27 @@
 n=1234
+list1=[]
+
 temp1=n%10
 temp2=n//10
 digit4=temp1
-print(digit4)
+list1.append(str(temp1))
 
 n=temp2
 temp1=n%10
 temp2=n//10
-digit3=temp1
-print(digit3)
+list1.append(str(temp1))
+
 
 n=temp2
 temp1=n%10
 temp2=n//10
-digit2=temp1
-print(digit2)
+list1.append(str(temp1))
+
 
 n=temp2
 temp1=n%10
 temp2=n//10
-digit1=temp1
-print(digit1)
+list1.append(str(temp1))
+print(list1)
+s1="".join(list1)
+print(s1)
