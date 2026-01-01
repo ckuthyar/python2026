805be800319d838590a10f77af52a28211960ebf
ckuthyar <ckuthyar@gmail.com>
2026-01-01 22:14:37 +0530

    Fibonacci series with For loop

--- 4.py
+++ 4.py
@@ -1,22 +1,10 @@
 list1=[1,1]
 
-pos=0
-item1=list1[pos]
-item2=list1[pos+1]
-item3=item1+item2
-list1.append(item3)
+for i in range(0,10,1):
+    pos=i
+    item1=list1[pos]
+    item2=list1[pos+1]
+    item3=item1+item2
+    list1.append(item3)
 print(list1)
 
-pos=1
-item1=list1[pos]
-item2=list1[pos+1]
-item3=item1+item2
-list1.append(item3)
-print(list1)
-
-pos=2
-item1=list1[pos]
-item2=list1[pos+1]
-item3=item1+item2
-list1.append(item3)
-print(list1)
